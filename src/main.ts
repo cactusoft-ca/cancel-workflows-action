f89a4735df4d5481c940b0e8420c55f572166e40
Mostefa Kamal Lala <56651912+MostefaKamalLala@users.noreply.github.com>
2021-07-21 14:31:41 -0400

    Printing pull requests

--- src/main.ts
+++ src/main.ts
@@ -76,6 +76,7 @@ async function main() {
 
         const branchWorkflows = data.workflow_runs.filter(function(run) {
           if(current_run.pull_requests?.length > 0){
+            console.log(`current_run.pull_requests ${JSON.stringify(current_run.pull_requests)}`)
             if(run.id !== current_run.id &&  run.pull_requests[0].id === current_run.pull_requests[0].id && run.status !== "completed"){
               return true
             }
