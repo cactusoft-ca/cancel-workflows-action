033b2e72ebda88e9e1a1d04e5d36757eb52efd60
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2022-06-17 10:20:20 -0400

    Printing all jobs

--- src/main.ts
+++ src/main.ts
@@ -15,17 +15,17 @@ async function main() {
     eventName,
     sha,
     ref,
-    repo: {owner, repo},
+    repo: { owner, repo },
     payload
   } = github.context
-  const {GITHUB_RUN_ID} = process.env
+  const { GITHUB_RUN_ID } = process.env
   core.debug(`GITHUB_RUN_ID ${GITHUB_RUN_ID}`)
   let branch = ref.slice(11)
   core.debug(`GITHUB_RUN_ID ${branch}`)
   let headSha = sha
   core.debug(`headSha ${headSha}`)
-  console.log(`payload.pull_request ${JSON.stringify(payload.pull_request)}`)
-  console.log(`payload.workflow_run ${JSON.stringify(payload.workflow_run)}`)
+  core.debug(`payload.pull_request ${JSON.stringify(payload.pull_request)}`)
+  core.debug(`payload.workflow_run ${JSON.stringify(payload.workflow_run)}`)
   if (payload.pull_request) {
     branch = payload.pull_request.head.ref
     headSha = payload.pull_request.head.sha
@@ -34,39 +34,39 @@ async function main() {
     headSha = payload.workflow_run.head_sha
   }
 
-  console.log({eventName, sha, headSha, branch, owner, repo, GITHUB_RUN_ID})
-  const token = core.getInput('github_token', {required: true})
-  const workflow_id = core.getInput('workflow_id', {required: false})
-  const ignore_sha = core.getInput('ignore_sha', {required: false}) === 'true'
-  console.log(`Found token: ${token ? 'yes' : 'no'}`)
+  core.debug(`${{ eventName, sha, headSha, branch, owner, repo, GITHUB_RUN_ID }}`)
+  const token = core.getInput('github_token', { required: true })
+  const workflow_id = core.getInput('workflow_id', { required: false })
+  const ignore_sha = core.getInput('ignore_sha', { required: false }) === 'true'
+  core.debug(`Found token: ${token ? 'yes' : 'no'}`)
   const workflow_ids: string[] = []
   const octokit = github.getOctokit(token)
 
-  const {data: current_run} = await octokit.actions.getWorkflowRun({
+  const { data: current_run } = await octokit.actions.getWorkflowRun({
     owner,
     repo,
     run_id: Number(GITHUB_RUN_ID)
   })
-  console.log(`current_run: ${current_run}`)
-  console.log(`workflow_id input: ${workflow_id}`)
+  core.debug(`current_run: ${current_run}`)
+  core.debug(`workflow_id input: ${workflow_id}`)
 
   if (workflow_id) {
     // The user provided one or more workflow id
-    workflow_id
-      .replace(/\s/g, '')
-      .split(',')
-      .forEach(n => workflow_ids.push(n))
+    const ids = workflow_id.replace(/\s/g, '').split(',')
+    for (const id of ids) {
+      workflow_ids.push(id)
+    }
   } else {
     // The user did not provide workflow id so derive from current run
     workflow_ids.push(String(current_run.workflow_id))
   }
 
-  console.log(`Found workflow_id: ${JSON.stringify(workflow_ids)}`)
+  core.debug(`Found workflow_id: ${JSON.stringify(workflow_ids)}`)
 
   await Promise.all(
     workflow_ids.map(async workflow_id => {
       try {
-        const {data} = await octokit.actions.listWorkflowRuns({
+        const { data } = await octokit.actions.listWorkflowRuns({
           owner,
           repo,
           workflow_id,
@@ -74,19 +74,19 @@ async function main() {
         })
         core.debug(`listWorkflowRuns: ${JSON.stringify(data)}`)
 
-        const branchWorkflows = data.workflow_runs.filter(function(run) {
-          if(!current_run) return;
-          if(!current_run.pull_requests) return;
-          if(current_run.pull_requests.length === 0) return;
-          const firstPr = current_run.pull_requests[0];
+        const branchWorkflows = data.workflow_runs.filter(function (run) {
+          if (!current_run) return
+          if (!current_run.pull_requests) return
+          if (current_run.pull_requests.length === 0) return
+          const firstPr = current_run.pull_requests[0]
 
-          if(!run) return;
-          if(!run.pull_requests) return;
-          if(run.pull_requests.length === 0) return;
+          if (!run) return
+          if (!run.pull_requests) return
+          if (run.pull_requests.length === 0) return
 
-          console.log(`current_run.pull_requests ${JSON.stringify(firstPr)}`)
+          core.debug(`current_run.pull_requests ${JSON.stringify(firstPr)}`)
 
-          if(run?.id !== current_run?.id &&  run?.pull_requests[0].id === firstPr.id && run?.status !== "completed"){
+          if (run?.id !== current_run?.id && run?.pull_requests[0].id === firstPr.id && run?.status !== "completed") {
             return true
           }
 
@@ -103,10 +103,23 @@ async function main() {
             new Date(run.created_at) < new Date(current_run.created_at)
         )
 
-        console.log(`%cwith ${runningWorkflows.length} runs to cancel.`, 'color: green;')
+        core.debug(`%cwith ${runningWorkflows.length} runs to cancel.`)
 
-        for (const {id, head_sha, status, html_url} of runningWorkflows) {
-          console.log('Canceling run: ', {id, head_sha, status, html_url})
+        // for each running workflows get the jobs that are in progress
+        const jobs = await Promise.all(
+          runningWorkflows.map(async run => {
+            const { data: jobData } = await octokit.actions.listJobsForWorkflowRun({
+              owner,
+              repo,
+              run_id: run.id
+            })
+            core.debug(`listJobsForWorkflowRun: ${JSON.stringify(jobData)}`)
+            return jobData.jobs
+          })
+        )
+
+        for (const { id, head_sha, status, html_url } of runningWorkflows) {
+          core.debug(`Canceling run: ${{ id, head_sha, status, html_url }}`)
           const res = await octokit.actions.cancelWorkflowRun({
             owner,
             repo,
